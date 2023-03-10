9b5a412f740d3edf7236f7da36c16471910b5058
Kamil <moskalakamil07@gmail.com>
2023-03-10 08:22:47 +0100

    fix: add meta tags in _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from "components/layout";
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 // import { Layout } from "pages";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -11,18 +12,27 @@ import { theme } from "styles/theme";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          {() => (
-            <Layout>
-              <Component {...pageProps} />
-              <GlobalStyle />
-            </Layout>
-          )}
-        </PersistGate>
-      </Provider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            {() => (
+              <Layout>
+                <Component {...pageProps} />
+                <GlobalStyle />
+              </Layout>
+            )}
+          </PersistGate>
+        </Provider>
+      </ThemeProvider>
+    </>
   );
 };
 export default appWithTranslation(App);
